8db8ad3e6e2faf46a6b0a0dd1b4e507491ddd2fb
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 22:03:23 +0900

    test : 문제 2 값은 쌍의 문자를 지워주는 기능 구현.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -8,7 +8,7 @@ public class Problem2 {
             System.out.println(e.getMessage());
             return "ERROR";
         }
-        String answer = "answer";
+        String answer = findPair(cryptogram);
         return answer;
     }
     private static void inputCheck(String crytogram) throws IllegalArgumentException {
@@ -19,10 +19,37 @@ public class Problem2 {
             throw new IllegalArgumentException("알파벳 소문자만 입력해주세요.");
         }
     }
-    public static String findPair(){
-        return "";
+    public static String findPair(String crytogram){
+        char[] crytogramChars = crytogram.toCharArray();
+        for(int i=0; i<crytogram.length()-1; i++){
+            if(crytogramChars[i]==crytogramChars[i+1] && crytogramChars[i] !='-'){
+                crytogramChars[i] ='-';
+                crytogramChars[i+1] = '-';
+                findOtherPair(crytogramChars, i);
+            }
+        }
+        String answer = String.valueOf(crytogramChars);
+        answer = answer.replaceAll("-","");
+        return answer;
     }
-    private static void findOtherPair(){
-
+    private static void findOtherPair(char[] crytogramChars, int i){
+        int right = i+2;
+        int left = i-1;
+        while(right<crytogramChars.length && left>=0){
+            if(crytogramChars[right]=='-'){
+                right++;
+                continue;
+            }
+            if(crytogramChars[left]=='-'){
+                left--;
+                continue;
+            }
+            if(crytogramChars[right]==crytogramChars[left]){
+                crytogramChars[right] = '-';
+                crytogramChars[left] = '-';
+            }
+            left--;
+            right++;
+        }
     }
 }
