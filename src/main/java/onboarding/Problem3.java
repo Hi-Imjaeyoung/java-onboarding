0b3424ad7752a3f28b7af6d73fb5e1d24e7f0cc5
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 22:21:16 +0900

    feat : 문제 3 예외 처리 기능 구현.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,15 +3,18 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         try{
-
+            inputCheck(number);
         }catch(IllegalArgumentException e){
-
+            System.out.println(e.getMessage());
+            return -1;
         }
         int answer = 0;
         return answer;
     }
-    private static void inputCheck(int number){
-
+    private static void inputCheck(int number) throws IllegalArgumentException{
+        if(number<1 || number >10_000){
+            throw new IllegalArgumentException("1이상 10,000이하 숫자를 입력해주세요");
+        }
     }
     public static int clapCalculate(int number){
 
