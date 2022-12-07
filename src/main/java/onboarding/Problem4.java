8f65771bfdeba8df0a203404c5cb82350fd26f9c
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 22:48:00 +0900

    feat : 문제 4 뼈대 코드 작성.

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
+        try{
+
+        }catch (IllegalArgumentException e){
+
+        }
         String answer = "";
         return answer;
+    }
+    private static void checkInput(){
+
+    }
+    public static String makePassword(){
+
+    }
+    public static String convert(){
+
     }
 }
