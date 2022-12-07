1ebafed20b4229f78241c301762331ba3ba52e94
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 11:18:24 +0900

    feat : 문제 1 뼈대 코드 작성.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        try{
+            checkInputNumbers(pobi);
+            checkInputNumbers(crong);
+        }catch(IllegalArgumentException e){
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
+
         return answer;
+    }
+    private static void checkInputNumbers(List<Integer> numbers) throws IllegalArgumentException{
+
+    }
+    private static void calculateMaxNumber(List<Integer> numbers){
+
     }
 }
