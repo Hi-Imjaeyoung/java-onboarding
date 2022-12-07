652325178299574ae762c927768c2a7c525d5d92
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 11:27:15 +0900

    feat : 입력값 예외처리 코드 작성.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -8,6 +8,7 @@ class Problem1 {
             checkInputNumbers(pobi);
             checkInputNumbers(crong);
         }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
             return -1;
         }
         int answer = Integer.MAX_VALUE;
@@ -15,7 +16,15 @@ class Problem1 {
         return answer;
     }
     private static void checkInputNumbers(List<Integer> numbers) throws IllegalArgumentException{
-
+        if(numbers.size() != 2){
+            throw new IllegalArgumentException("2개의 숫자만 입력해주세요");
+        }
+        if(numbers.get(0)+1 != numbers.get(0)){
+            throw new IllegalArgumentException("연속되는 두 자연수만 입력해주세요");
+        }
+        if(numbers.get(0)<0 || numbers.get(0)>399 || numbers.get(1)<1 || numbers.get(1)>400){
+            throw new IllegalArgumentException("1과 400 사이의  두 자연수만 입력해주세요");
+        }
     }
     private static void calculateMaxNumber(List<Integer> numbers){
 
