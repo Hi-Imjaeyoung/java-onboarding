07a25df4c872bb9f3bc74a0ab6cf2291669f50e9
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 23:41:00 +0900

    feat : 문제 5 입력값 예외 처리 기능 구현.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -4,8 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    static final List<Integer> papers = List.of(50_000,10_000,5_000,1_000,500,100,50,10,1);
     public static List<Integer> solution(int money) {
+        try {
+            checkInput(money);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return Collections.emptyList();
+        }
         List<Integer> answer = Collections.emptyList();
         return answer;
+    }
+    private static void checkInput(int input) throws IllegalArgumentException{
+        if(input<1 || input >1_000_000){
+            throw new IllegalArgumentException("1이상 1,000,000 숫자를 입력하세요");
+        }
+    }
+    public static List<Integer> calculate(int money){
+
     }
 }
