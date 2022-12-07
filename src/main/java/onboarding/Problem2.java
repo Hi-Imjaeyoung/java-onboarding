5ee738cc48b78bbd5a949a9c16495ab058c8b407
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 21:26:35 +0900

    feat : 문제 2 입력값 예외 확인 기능 구현.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,11 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
+        try{
+            inputCheck(cryptogram);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return "";
+        }
         String answer = "answer";
         return answer;
     }
-    private static void inputCheck(String crytogram){
-
+    private static void inputCheck(String crytogram) throws IllegalArgumentException {
+        if(crytogram.length()==0 || crytogram.length()>1000){
+            throw new IllegalArgumentException("1에서 1000자리 사이의 문자를 입력해주세요,");
+        }
+        if(crytogram.replaceAll("^[a-z]","").length() != crytogram.length()){
+            throw new IllegalArgumentException("알파벳 소문자만 입력해주세요.");
+        }
     }
     public static String findPair(){
 
