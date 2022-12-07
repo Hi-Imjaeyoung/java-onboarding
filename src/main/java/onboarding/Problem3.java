f2bce0f6bbe3be48ad5a74efbba31030b5967efe
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-07 22:19:31 +0900

    feat : 문제 3 뼈대 코드 작성.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
+        try{
+
+        }catch(IllegalArgumentException e){
+
+        }
         int answer = 0;
         return answer;
+    }
+    private static void inputCheck(int number){
+
+    }
+    public static int clapCalculate(int number){
+
     }
 }
