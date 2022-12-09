d0fa673ec3bb90e1bd55d21a578b56e266b09f6f
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-09 23:36:51 +0900

    feat : 문제 6 입력에 따른 예외 처리 기능 구현.

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -12,8 +12,21 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
-    private static void checkInput(List<List<String>> forms) {
-
+    public static void checkInput(List<String> form) throws IllegalArgumentException{
+        String email = form.get(0);
+        String nickName = form.get(1);
+        if(!email.contains("@email.com")){
+            throw new IllegalArgumentException("이메일 형식을 입력해주세요");
+        }
+        if(email.length()<11 || email.length()>=20){
+            throw new IllegalArgumentException("이메일은 11자 이상 20자 미만입니다");
+        }
+        if(nickName.replaceAll("[^가-힣]","").length()!=nickName.length()){
+            throw new IllegalArgumentException("닉네임은 한글만 가능합니다");
+        }
+        if(nickName.length()==0 || nickName.length()>=20){
+            throw new IllegalArgumentException("1자 이상 20자 미만의 닉네임을 입력해주세요");
+        }
     }
 
     public static List<String> makeToken(String nickname){
