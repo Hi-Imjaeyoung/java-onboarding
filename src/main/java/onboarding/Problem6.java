f4aad24885e60645cbb72a3d675a85c928758f82
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-09 23:28:04 +0900

    feat : 문제 6 뼈대 코드 작성

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,28 @@
 package onboarding;
 
 import java.util.List;
-
+import java.util.Set;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
+        try{
+
+        }catch{
+
+        }
         List<String> answer = List.of("answer");
         return answer;
     }
+    private static void checkInput(List<List<String>> forms) {
+
+    }
+
+    public static List<String> makeToken(String nickname){
+
+    }
+
+    public static void sortEmail(Set<String> emails){
+
+    }
+
 }
+
