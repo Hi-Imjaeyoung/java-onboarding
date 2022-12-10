38ca9b08a9348538f5aad22733c96011a2f6468b
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 16:23:21 +0900

    feat : 문제 7 친구관계를 HashMap에 저장하는 기능 구현.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,6 +1,8 @@
 package onboarding;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem7 {
@@ -21,8 +23,23 @@ public class Problem7 {
             throw new IllegalArgumentException("알파벳 소문지로된 아이다만 입력해주셍요");
         }
     }
-    public static void calculateScoreAtFriends(){
-
+    public static HashMap<String,List<String>> calculateScoreAtFriends(List<List<String>> friends){
+        HashMap<String,List<String>> friendship = new HashMap<>();
+        for(List<String> friend : friends){
+            if(friendship.containsKey(friend.get(0))){
+                friendship.get(friend.get(0)).add(friend.get(1));
+            }else{
+                friendship.put(friend.get(0),new LinkedList<>());
+                friendship.get(friend.get(0)).add(friend.get(1));
+            }
+            if(friendship.containsKey(friend.get(1))){
+                friendship.get(friend.get(1)).add(friend.get(0));
+            }else{
+                friendship.put(friend.get(1),new LinkedList<>());
+                friendship.get(friend.get(1)).add(friend.get(0));
+            }
+        }
+        return friendship;
     }
     public static void calculateScoreAtVisitors(){
 
