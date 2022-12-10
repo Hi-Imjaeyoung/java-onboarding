ea6b31449aec9eafa5e8517b2ec61d09498e01de
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 16:10:28 +0900

    feat : 문제 7 뼈대 코드 작성.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -5,7 +5,24 @@ import java.util.List;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
+        try{
+
+        }catch (IllegalArgumentException e){
+
+        }
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+    public static void checkInput(){
+
+    }
+    public static void calculateScoreAtFriends(){
+
+    }
+    public static void calculateScoreAtVisitors(){
+
+    }
+    public static void sortRecommendFriends(){
+
     }
 }
