30f5bc74db95a8a6e15af526fcad7bac0a4d5670
Hi-Imjaeyoung <imjy2757@naver.com>
2022-12-10 16:13:46 +0900

    feat : 문제 7 입력 예외 처리코드 작성.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -13,8 +13,13 @@ public class Problem7 {
         List<String> answer = Collections.emptyList();
         return answer;
     }
-    public static void checkInput(){
-
+    public static void checkInput(String id) throws IllegalArgumentException{
+        if(id.length()==0 || id.length()>30){
+            throw new IllegalArgumentException("1자 이상 30자 이하의 아이디를 입력해주세요.");
+        }
+        if(id.replaceAll("[^a-z]","").length() != id.length()){
+            throw new IllegalArgumentException("알파벳 소문지로된 아이다만 입력해주셍요");
+        }
     }
     public static void calculateScoreAtFriends(){
 
